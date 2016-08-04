ca497ff8c5177ae4ae7d1d2e560c0621c8e9bd51
Sridevi <sridevi9akula@gmail.com>
2016-08-03 19:36:39 -0700

    File name added

--- app/src/main/java/com/app/sri/fragmentcreating/MainActivity.java
+++ app/src/main/java/com/app/sri/fragmentcreating/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+/*
+File name MainActivity.Java
+*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
